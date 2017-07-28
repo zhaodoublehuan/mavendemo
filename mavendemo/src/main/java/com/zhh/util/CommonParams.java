893a4f3f8727a7246c278f5f49bc4e0e1d217f70
z702324140 <z702324140@af755ebe-e123-4d7d-95ec-f192d4465a51>
2017-07-28 06:31:50 +0000

    修改描述

--- mavendemo/src/main/java/com/zhh/util/CommonParams.java
+++ mavendemo/src/main/java/com/zhh/util/CommonParams.java
@@ -1,8 +1,9 @@
 package com.zhh.util;
 
 public class CommonParams {
-	/*用户状态有效*/
+	/*状态有效*/
 	public final static String ACTIVE="Y";
-	/*用户状态无效*/
+	/*状态无效*/
 	public final static String NO_ACTIVE="N";
+	      
 }
