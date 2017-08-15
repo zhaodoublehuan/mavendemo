66502b1e3eebd59222c979088f59b0d330882a95
z702324140 <z702324140@af755ebe-e123-4d7d-95ec-f192d4465a51>
2017-08-15 06:04:13 +0000

    新增异常类

---
+++ mavendemo/src/main/java/com/zhh/exception/UserException.java
@@ -0,0 +1,18 @@
+package com.zhh.exception;
+
+public class UserException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2100003732313677862L;
+	
+	public UserException(){
+		super();
+	}
+	
+	public UserException(String msg){
+		super(msg);
+	}
+
+}
